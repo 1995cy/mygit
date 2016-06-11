c8142454282db26d03da9a532faa5cb3b914d9f1
cy <827170272@qq.com>
2016-06-11 15:13:40 +0800

    f

--- http_download/resolve.h
+++
@@ -1,7 +0,0 @@
-#ifndef RESOLVE_H_INCLUDED
-#define RESOLVE_H_INCLUDED
-
-int str_resolve(char *url_name,char *host_name,char *path_filename,ushort *port);
-int port_resolve(char *start,char *strend,ushort *port);
-
-#endif // RESOLVE_H_INCLUDED
